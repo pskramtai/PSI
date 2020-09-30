17a617225ccd753bec440c03354f88c6ff177133
Paulius <Paulius@DESKTOP-HLLHU62>
2020-09-30 19:36:01 +0300

    JSON parse methods

--- Comparison Engine/Comparison Engine/Data/Data.cs
+++ Comparison Engine/Comparison Engine/Data/Data.cs
@@ -1,9 +1,11 @@
 ﻿using Comparison_Engine.Base_Classes;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Comparison_Engine
 {
@@ -12,40 +14,18 @@ namespace Comparison_Engine
 
         public static List<Bar> GetBars()
         {
-            string filePath = Path.Combine(Environment.CurrentDirectory, @"testDataBars.txt");
-            string[] lines = File.ReadAllLines(filePath);
-            if (lines == null || lines.Length == 0) return null;
+            StreamReader r = new StreamReader("barInput.json");
+            string json = r.ReadToEnd();
+            List<Bar> bars = JsonConvert.DeserializeObject<List<Bar>>(json);
+            return bars;
+        }
 
-            List<Bar> barList = new List<Bar>();
-            int barId;
-            string barName;
-            string barLocation;
-
-            int drinkId;
-            float drinkPrice;
-
-            foreach(string line in lines)
-            {
-                //Splitting line into pieces to build bar object. First 3 pieces are ID, name and location.
-
-                string[] pieces = line.Split(',');
-                barId = Int32.Parse(pieces[0]);
-                barName = pieces[1];
-                barLocation = pieces[2];
-
-                Bar bar = new Bar(barId, barName, barLocation);
-
-                //the remaining pieces are drink IDs and prices in pairs.
-
-                for(int i=3; i<pieces.Length; i += 2)
-                {
-                    drinkId = Int32.Parse(pieces[i]);
-                    drinkPrice = float.Parse(pieces[i + 1]);
-                    bar.AddDrink(drinkId, drinkPrice);
-                }
-                barList.Add(bar);
-            }
-            return barList;
+        public static List<Bar> GetDrinks()
+        {
+            StreamReader r = new StreamReader("drinkInput.json");
+            string json = r.ReadToEnd();
+            List<Bar> drinks = JsonConvert.DeserializeObject<List<Bar>>(json);
+            return drinks;
         }
 
     }
