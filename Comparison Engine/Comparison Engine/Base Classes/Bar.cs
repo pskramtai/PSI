7e45bced2f3b472a3008dfbb8ba161e77ba42a50
Kasparas Jankevičius <kasparas.jankevicius@unity3d.com>
2020-10-01 10:41:35 +0300

    Cleaned up Bar.cs and Drink.cs to only have the essential info on those things. Added BarManager.cs and DrinkManager.cs to handle bars and drinks respectively

--- Comparison Engine/Comparison Engine/Base Classes/Bar.cs
+++ Comparison Engine/Comparison Engine/Base Classes/Bar.cs
@@ -1,39 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Comparison_Engine.Base_Classes
 {
-    class Bar
+    public class Bar
     {
         //ID of the bar
         public int barID { get; set; } 
 
         //Actual name of the bar
-        public String barName { get; set; } 
+        public string barName { get; set; } 
 
         //Location of the bar
-        public String location { get; set; } 
+        public string location { get; set; } 
 
-        public Bar(int barID, String barName, String location)
+        public Bar(int barID, string barName, string location)
         {
             this.barID = barID;
             this.barName = barName;
             this.location = location;
         }
 
-        //A dictionary of drinks available at this bar along with the prices, int for drink ID, float for price
-        public Dictionary<int, float> availableDrinks = new Dictionary<int, float>();
-
-        public void AddDrink(int drinkID, float price)
-        {
-            availableDrinks.Add(drinkID, price);
-        }
-
-        public float FindDrinkPrice(int drinkID)
-        {
-            //Return the price of specified drink at this bar
-            return availableDrinks.ContainsKey(drinkID) ? availableDrinks[drinkID] : -1; //Should return something else besides -1 if drink not available here
-        }
+        //A dictionary of drinks available at this bar along with the prices
+        public Dictionary<Drink, float> availableDrinks = new Dictionary<Drink, float>();
     }
 }
---
+++ Comparison Engine/Comparison Engine/Base Classes/BarManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Comparison_Engine.Base_Classes
+{
+    public sealed class BarManager
+    {
+        private static readonly Lazy<BarManager> lazy = new Lazy<BarManager>(() => new BarManager());
+
+        public static BarManager Instance { get { return lazy.Value; } }
+
+        private BarManager()
+        {
+
+        }
+
+        //A list of bars
+        public List<Bar> barList = new List<Bar>();
+
+        //Adds a new bar
+        public void AddBar(string barName, string location)
+        {
+            int barID = barList.Count - 1;
+            barList.Add(new Bar(barID, barName, location));
+        }
+
+        //Adds a drink to the bar's menu
+        public void AddDrink(int barID, Drink drink, float price)
+        {
+            barList[barID].availableDrinks.Add(drink, price);
+        }
+
+        //Finds the price of a specific drink at a specific bar
+        public float FindDrinkPrice(int barID, Drink drink)
+        {
+            //Return the price of specified drink at this bar
+            return barList[barID].availableDrinks.ContainsKey(drink) ? barList[barID].availableDrinks[drink] : -1; //Should return something else besides -1 if drink not available here
+        }
+    }
+}
