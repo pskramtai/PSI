34d3d10b0c90726ee9c9431d57f1356ec085a709
Paulius <Paulius@DESKTOP-33IFKNL>
2020-10-22 00:28:12 +0300

    Added search functions

---
+++ Comparison Engine/Comparison Engine/SearchClass.cs
@@ -0,0 +1,37 @@
+﻿using Comparison_Engine.Base_Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comparison_Engine
+{
+    class SearchClass
+    {
+        public static List<Bar> SearchForBars(List<Bar> currentBarList, String keyword)
+        {
+            List<Bar> bars = new List<Bar>();
+            foreach(Bar bar in currentBarList)
+            {
+                if(bar.barName.ToLower().Contains(keyword.ToLower()))
+                {
+                    bars.Add(bar);
+                }
+            }
+            return bars;
+        }
+
+
+        public static List<Drink> SearchForDrinks(List<Drink> currentDrinkList, String keyword)
+        {
+            List<Drink> drinks = new List<Drink>();
+            foreach (Drink drink in currentDrinkList)
+            {
+                if(drink.drinkName.ToLower().Contains(keyword.ToLower()))
+                {
+                    drinks.Add(drink);
+                }
+            }
+            return drinks;
+        }
+    }
+}
