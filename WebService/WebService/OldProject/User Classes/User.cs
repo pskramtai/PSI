f750374eeb853e651d15d54db2b87969ac3b2787
Elektros Stulpas <aurimas0pupsys@gmail.com>
2020-11-18 19:58:36 +0200

    added a web service project and included our old base classes

---
+++ WebService/WebService/OldProject/User Classes/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Comparison_Engine.Base_Classes;
+
+namespace Comparison_Engine.User_Classes
+{
+    public sealed class User
+    {
+        private static readonly Lazy<User> lazy = new Lazy<User>(() => new User());
+
+        public static User Instance { get { return lazy.Value; } }
+
+        //some general variables that will likely be needed for the User class
+        public string username { get; set; }
+
+        public Bar favoriteBar { get; set; }
+
+        public string currentLocation { get; set; }
+
+        //Enum defined in UserTrustLogic.cs
+        public UserTrustLogic.UserTrustType userTrust { get; set; }
+
+        private User()
+        {
+            username = "Guest";
+            userTrust = UserTrustLogic.UserTrustType.None;
+        }
+
+        public void ChangeUsername(string newUsername)
+        {
+            username = newUsername;
+            SaveUserInfo();
+        }
+
+        public void ChangeFaveBar(Bar newBar)
+        {
+            favoriteBar = newBar;
+            SaveUserInfo();
+        }
+
+        public void ChangeCurrentLoaction(string newLocation) //will need to talk about how this should be handled
+        {
+
+        }
+
+        public void ChangeUserTrust(UserTrustLogic.UserTrustType newTrust)
+        {
+            userTrust = newTrust;
+            SaveUserInfo();
+        }
+
+        public void SaveUserInfo() //will need to save user data somewhere
+        {
+
+        }
+    }
+}
