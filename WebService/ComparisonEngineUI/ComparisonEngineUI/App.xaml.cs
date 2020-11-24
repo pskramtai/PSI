3afaa66d791b2f97fea7e11c04c26511d973210d
Raimond <rsobolevskij@gmail.com>
2020-11-24 19:31:45 +0200

    Added empty xamarin project

---
+++ WebService/ComparisonEngineUI/ComparisonEngineUI/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ComparisonEngineUI.Services;
+using ComparisonEngineUI.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ComparisonEngineUI
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ WebService/ComparisonEngineUI/ComparisonEngineUI/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ComparisonEngineUI.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ComparisonEngineUI.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
