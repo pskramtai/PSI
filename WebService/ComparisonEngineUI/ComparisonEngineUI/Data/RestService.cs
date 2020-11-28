3687d3955aff6f5332a7de004a6dd6dfe7cda875
Paulius <Paulius@DESKTOP-QS62O8H>
2020-11-28 14:33:22 +0200

    one more namespace update

--- WebService/ComparisonEngineUI/ComparisonEngineUI/Data/RestService.cs
+++ WebService/ComparisonEngineUI/ComparisonEngineUI/Data/RestService.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using WebService.Base_Classes;
+using ComparisonEngineUI.Models;
 
 namespace ComparisonEngineUI.Data
 {
