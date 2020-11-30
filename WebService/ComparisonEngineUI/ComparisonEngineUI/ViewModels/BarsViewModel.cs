60f3987f4ea5180445819bff0faaabd8821fb35a
Paulius <Paulius@DESKTOP-QS62O8H>
2020-11-30 13:22:15 +0200

    fixing connectivity issues

--- WebService/ComparisonEngineUI/ComparisonEngineUI/ViewModels/BarsViewModel.cs
+++ WebService/ComparisonEngineUI/ComparisonEngineUI/ViewModels/BarsViewModel.cs
@@ -5,6 +5,9 @@ using ComparisonEngineUI.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ComparisonEngineUI.Views;
+using ComparisonEngineUI.Data;
+using ComparisonEngineUI.Models;
+using System.Threading.Tasks;
 
 namespace ComparisonEngineUI.ViewModels
 {
@@ -12,14 +15,15 @@ namespace ComparisonEngineUI.ViewModels
     class BarsViewModel : BaseViewModel
     {
         public Command EditBarCommand { get; }
+        public List<Bar> bars { get; set; }
         public BarsViewModel()
         {
-            EditBarCommand = new Command(OnEditBarClicked);
+            EditBarCommand = new Command(OnEditBarClicked);        
         }
         private async void OnEditBarClicked(object obj)
         {
             await Shell.Current.GoToAsync($"{nameof(EditPage)}");
         }
-
+        
     }
 }
