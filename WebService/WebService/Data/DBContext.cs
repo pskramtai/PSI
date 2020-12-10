5aedd2b444589532a48191eb642a98e0ecc2a474
Elektros Stulpas <aurimas0pupsys@gmail.com>
2020-12-10 02:23:45 +0200

    leaving the OnModelCreating binds in since they are required for migration, but commented, since they produce an error, because bar object is no longer referenced in SpecificPrice

--- WebService/WebService/Data/DBContext.cs
+++ WebService/WebService/Data/DBContext.cs
@@ -21,6 +21,15 @@ namespace WebService.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            /*modelBuilder.Entity<Bar>()
+                .HasMany<SpecificPrice>(b => b.AvailableDrinks)
+                .WithOne(s => s.Bar)
+                .HasForeignKey(s => s.BarID);
+
+            modelBuilder.Entity<Drink>()
+                .HasMany<SpecificPrice>(d => d.DrinkLocations)
+                .WithOne(s => s.Drink)
+                .HasForeignKey(s => s.DrinkID);*/
         }
 
         public DbSet<Drink> Drink { get; set; }
